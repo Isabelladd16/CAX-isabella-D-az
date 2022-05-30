a1d3380c7f32fe918d607d2b75eaa0c7b2808900
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-05-30 09:14:35 -0500

    Add files via upload

---
+++ Python/Primer Trimestre/Hola.py
@@ -0,0 +1,6 @@
+print('hi im isabella')
+print('im in 9th C')
+print('my favorite food is white sushi rice')
+print('i love doing sports')
+print('during the summer i was in a summer camp in maine called camp Vega')
+
