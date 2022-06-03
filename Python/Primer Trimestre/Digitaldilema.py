ed986873e6ee5f1ccaa4d31171fc80d622ba2e29
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-03 13:51:41 -0500

    Add files via upload

---
+++ Python/Primer Trimestre/Digitaldilema.py
@@ -0,0 +1,27 @@
+print(how can we act with empathy and positivity when we are online?)
+print(by not sending hate comments or messages to other people cause you can really hurt their feelings)
+
+print(what makes this a digital dilemma?)
+print (what they show in the video about humiliating people i dotn really understand why they do it and i dont like it when people do that) 
+
+print(what dilemma or challenge are they facing?)
+print(the people where the bullies, the boy and the girl, amd the problem was either to post the letter or not)
+
+print(What do you think each person in the dilemma is feeling? Why might the situation be hard or challenging for each of them?)
+print(the girl is feeling guilty and they boy is being bullied)
+
+print(where most people feel good or taken care of?)
+print(the girld didn't have to post the letter she could have kept it for herself)
+
+print(what could the people involved say?)
+print(that the letter is private)
+
+
+
+
+
+
+
+
+
+
