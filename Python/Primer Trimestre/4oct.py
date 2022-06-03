8fa599a0676092165949f1d2b2989bd67041280b
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-03 13:45:14 -0500

    Add files via upload

---
+++ Python/Primer Trimestre/4oct.py
@@ -0,0 +1,22 @@
+#ejemplos de constantes
+x=10
+jamon=10
+justo=2345
+excusa='puedo ir al baño?'
+print(x)
+print(jamon)
+print(excusa)
+
+c0m03st4n=20
+print(c0m03st4n)
+print('el precio del jamon es:', jamon)
+
+name='Isabella Diaz'
+grade='9C'
+food='white sushi rice'
+tell ='i have 4 golf tournaments in october'
+
+print('my name is' , name)
+print('im in' , grade)
+print('my favorite food is' , food)
+print('guess what', tell)
