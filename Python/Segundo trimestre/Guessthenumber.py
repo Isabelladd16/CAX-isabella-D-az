66ba19c0e96efc8f8ab599737b2622dcc0167f0c
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-03 14:02:20 -0500

    Add files via upload

---
+++ Python/Segundo trimestre/Guessthenumber.py
@@ -0,0 +1,34 @@
+cal=int(input('test:'))
+
+if(cal<11):
+  
+  print('fail')
+
+if(test>11):
+  
+  print('pass')
+
+
+if(test>13):
+ 
+  print('bonus')
+
+  print("done")
+ 
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
