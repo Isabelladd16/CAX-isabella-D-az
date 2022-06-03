2ed9306328bc2d18b5af14987ba3344b82e8b335
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-03 13:53:53 -0500

    Add files via upload

---
+++ Python/Primer Trimestre/Codefortheweek.py
@@ -0,0 +1,24 @@
+numero1=int(input('escriba su primer medida: '))
+numero2=int(input('escriba su segunda medida:'))
+numero3=int(input('escriba su tercera medida (si la figura no tiene esta medida pon el numero 1):'))
+
+numero4=int(input('escriba su cuarta medida (si no tiene esta medida ponga el numero 1):'))
+
+resultado1=numero1*numero2/numero3*numero4
+resultado2=numero1*numero2*numero3*numero4
+resultado3=numero1*numero2*numero3/numero4
+
+print ('el area de un triangulo es:',resultado1)
+print ('el area de un rectangulo es:', resultado2)
+print ('el area de un pentagono es:', resultado3)
+
+
+
+
+
+
+
+
+
+
+
