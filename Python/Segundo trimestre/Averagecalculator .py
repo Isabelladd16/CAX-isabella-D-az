b6c5e5f18f82898f2f0789884c28e7fbabc82c53
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-03 14:03:39 -0500

    Add files via upload

---
+++ Python/Segundo trimestre/Averagecalculator .py
@@ -0,0 +1,35 @@
+void loop()
+{
+  num=int(input('misterious number:'))
+
+if(num<4):
+  print('to small')
+
+if(num=4):
+  print("correct")
+
+if(num>4):
+  print('really high')
+
+print("done")
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+}
