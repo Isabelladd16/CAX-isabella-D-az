b794e1a0b13250c7d2805e82fd6f5ce3158c8249
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-05 21:18:13 -0500

    Add files via upload

---
+++ Python/Segundo trimestre/IntFunction.py
@@ -0,0 +1,16 @@
+numero=int(input('ingresa un numero:'))
+numero2=int(input('ingresa un segundo numero:'))
+
+resultado=numero+numero2
+print('el resultado de la suma es:', resultado)
+
+
+resultado=numero-numero2
+print('el resultado de la resta es:', resultado)
+
+resultado=numero*numero2
+print('el resultado de la multiplicacion es:', resultado)
+
+
+resultado=numero/numero2
+print('el resultado de la división es:', resultado)
