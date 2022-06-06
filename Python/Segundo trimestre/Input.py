3479b016ba166f01af7c6e238b2487973a4c52c2
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-05 21:16:18 -0500

    Add files via upload

---
+++ Python/Segundo trimestre/Input.py
@@ -0,0 +1,38 @@
+print('what is your name?')
+nombre=input()
+print('hi', nombre)
+
+grade=input('what grade are you in?')
+print('im also in', grade,)
+ 
+print('what is your favorite food?')
+thing=input()
+print('yummy!!', thing)
+
+print('how old are you?')
+age=input()
+print('wow!')
+
+print('where are you from?')
+place=input()
+print('that is really cool')
+
+print('do you have any sibilings?')
+sibilings=input()
+print('that is cool')
+
+print('do you have pets?')
+pets=input()
+print('same!')
+
+print('have you read any books lately?')
+books=input()
+print('oh okey')
+
+print('what is yout favorite type of music ?')
+music=input()
+print('i really like',music, 'too')
+
+print('anything else you want to tell me?')
+tell=input()
+print('okey, it was nice chating with you')
