abc23812fae820220a472cfb06a9d4a2ff044807
Isabelladd16 <104370387+Isabelladd16@users.noreply.github.com>
2022-06-13 13:43:05 -0500

    Add files via upload

---
+++ Python/Tercer trimestre/Rosabello.py
@@ -0,0 +1,528 @@
+print("Welcome to the game ROSABELLO what name you would like to use?")
+
+input()
+name=input()
+#input()
+print("hi",name)
+
+print("what difficulty you would like to play, easy, medium or difficult")
+
+input()
+print("ok, wish you the best")
+
+print("which character are you choosing?,you can choose whatever you want")
+input()
+print("you are ready to fight,good luck")
+input()
+
+print("Once upon a time there was a group of friends who were going on an school trip")
+      
+print("to the zoo, when they got off the bus and manage to see a book that shines as if it had a light inside, ")
+      
+print("they decide to open the book, and at the moment they appear in another dimension")
+      
+print("then they continued with the path back home and they will have to do what some quests to return")
+
+input()
+
+print("to get home you can choose two paths, the longer but safer or the short one but with a lot of quests")
+
+input()
+
+print("okkkkkk, but are you sure?")
+
+input()
+
+print("here we go...")
+
+print("you just found a donkey and a cow on your way home, wich one do you want to choose")
+
+option=input("a)donkey and b) cow")
+
+if(option=="a"):
+  print("you might not get home fast because the donkey is not smart and it will go way slower")
+
+
+if(option=="b"):
+  print("congratulations you chose whisely, the cow is smarter but unfortunately you will go slower")
+
+print("you have been riding on the cow for a few hours")
+
+print("but something is coming...")
+
+print("not that far away from you, a dragon with her babies")
+  
+print("the dragon is getting closer and closer until you have to make a choice")
+
+print("you have 2 options one is fight the dragons and the second one is to try to scape")
+
+print("choose whisley...")
+
+option=input("a) fight or b) scape")
+
+if (option=="a"):
+ print("you have lost the battle")
+ print("now you have to run")
+ print("or else you will die")
+
+
+if (option=="b"):
+ print("that is a smart desition")
+ print("now you have to start running")
+ print("run faster!!")
+ print("you are safe for now")
+
+print("now that you are safe from the dragos you have to keep going your way")
+
+
+print("you have been going for a few hours now and nothing has seemed to change, keep going")
+
+print("now as you are riding your animal you think you have seen your house")
+
+print("you get really excited and happy because it has been quite the adventure")
+
+print("you keep getting closer and closer")
+
+print("you just got close enogh to see that it is not your house")
+
+print("you get close to the door and you have decided to knock")
+
+print("an ogre opens the door and tells you to come in, what is your chice?")
+
+choice=input("a)go in or b)leave")
+
+if (choice=="a)"):
+ print("you have made not a very smart choice ")
+ print("you are now inside the house")
+ print(" the ogre tells you if you want something to drink")
+ print("what is going to be your answer?")
+ answer=input("a)yes thanks or b)no thanks im fine")
+  
+ if (answer=="yes"):
+   print("you drink the water and you start to feel sick")
+   print("you decide to tell the ogre")
+   print("but then the ogre tells you that its normal")
+   print("after a few minutes of feeling sick you start to feel fine again")
+   if (answer=="no"): 
+    print("the ogre tells you that if you dont want anything to drink you should leave")
+   print("so then you thank him and you leave the house")
+
+if (choice=="b)"):
+ print("you tell the ogre that thanks but that you need to keep going")
+ print("the ogre trys to stop you")
+ print("you get your animal and ride away")
+
+
+print("you keep riding for a few hours")
+
+print("but now its getting late and you dont have a light for when its dark")
+
+print("you have decided to keep riding until night falls")
+
+print("......")
+
+print("now the night has fallen and its time for you to make a new choice")
+
+print("what would you like to do now?, your options are 1 find a place to sleep, 2 dont sleep and try to find your house or 3 stay awake but dont look for your house")
+
+options=input("1, 2 or 3")
+
+if (option=="1"):
+ print("smart choice!")
+ print("you need to rest, lets find a place")
+ print("look at that tree, it looks comfortable ")
+ print("now that you are almos asleep we just have to wait for the new day...")
+
+if (option=="2"):
+ print("good choice but not the best")
+ print("but anyway lets look for your house")
+ print("its getting darker and you cant see where your are walking")
+ print("after a while of looking the sun starts tu rise...")
+
+if (option=="3"):
+ print("this is the dumbest choice you have ever made")
+ print("but i will keep you company")
+ print("let your animal rest while we talk")
+ print("a few hours later...")
+ print("now you can start to see the sun rise")
+
+
+print("good morning, it seems like a good day to start a new adventure")
+
+print("wake your animal so we can keep going")
+
+print("okey now lets keep going")
+
+print("as you keep riding your animal lays down and doesnt want to walk anymore")
+
+print("i think your animal is hungry he hasnt eaten anything since you chose him")
+
+print("do you want to look for food?")
+
+answer=input("yes or no")
+
+if (answer=="yes"):
+ print("okey i think i saw an apple tree back there")
+ print("lets go look for it")
+ print("okey now jump end get them")
+ print("now that we have the apples lets go back so he can eat")
+
+if (answer=="no"):
+ print("fine then you should leave the animal")
+ print("or its going to be difficult to keep going with an animal that cant walk")
+ print("i dont like your choice but i cant change it")
+ print("lets keep walking")
+
+print("now that you have made your choice lets keep going...")
+
+print("a few hours have past but you still see nothing...")
+
+print("you keep going and going but there is nothing")
+
+print("you cant see anything")
+
+print("you are just able to see trees and hear animals")
+
+print("after a while you start to see that the path is going to separate")
+
+print("no one knows where they go they just call them path a) and path be) make your choice")
+
+choice=input("a) or b)")
+
+if (choice=="a)"):
+ print("i dont know where we are going but it seems like a good choice")
+ print("lets keep going")
+
+if (choice=="b)"):
+ print("i dont know if its good or bad...")
+ print("lets find out")
+ print("walk that way")
+
+print("i think you have chosen whisely")
+
+print("you are really smart")
+
+print("so far there is not anything out of normal")
+
+
+print("just trees and you can hear animal sounds")
+
+print("but no sign of any buildings")
+
+print("lets keep looking...")
+
+print("a few hours have passed and nothing has changed")
+
+print("just keep going for a few hours")
+
+print("...")
+
+print("i think we are getting closer to society")
+
+print("i think there is a village not far away from here")
+
+print("lets keep going towards that way")
+
+print("now you have started to see the village")
+
+print("you have started to see houses and farms")
+
+print("you keep getting closer and closer")
+
+print("until you can see people around")
+
+print("do you want to ask them where you are?")
+
+ask=input("yes or no")
+
+if (ask=="yes"):
+  print("okey then lets go ask")
+  print("look at tha woman she looks nice")
+  print("lets go and ask her")
+  print("after you ask her she says")
+  print("you are in Rosabell")
+  print("you can stay in my house")
+  print("im hungry can we eat?")
+
+if (ask=="no"):
+  print("okey then lets find out where we are")
+  print("look for signs or a map")
+  print("i think there is one to your right")
+  print("look it says welcome to Rosabell")
+  print("okey now that we know where we are lets finda place to eat")
+  print("you must be hungry")
+
+print("okey lets find a place to eat")
+
+print("i saw a restaurant a few minutes ago")
+
+print("it was that way...")
+
+print("lets go!!!")
+
+print("it is a little far")
+
+print("we have walked a lot and havent seen anything yet")
+
+print("lets go back to the village")
+
+print("i think the village is that way")
+
+print("or maybe to other way")
+
+print("which way do you want to go?")
+
+way=input("a) or b)")
+
+if (way=="a)"):
+  print("lets fin out where this road goes")
+  print("i think it does go back to the village")
+  print("very good chice")
+
+if (way=="b)"):
+  print("lets see where this takes us")
+  print("you are smart ")
+  print("i think im starting to see the village")
+  print("lets keep going")
+
+print("now you have started to see the village")
+
+print("lets go!")
+
+print("we are almost there")
+
+print("you have got this")
+
+print("finally we are back in the village")
+
+print("now you feel safe")
+
+print("another day has passed")
+
+print("i think you are getting close to your home")
+
+print("look a map!, take it")
+
+print("okey now lets go back home")
+
+print("you guide the way back")
+
+print("okey first lets start walking ")
+
+print("your first choice is coming up...")
+
+print("look there is three different paths")
+
+print("the firts is full or rocks and any time they can fall")
+
+print("the second one is a river and there is a small boat but it is really small and not that strong")
+
+print("and finally the third one is a normal path its just full of dangerous animals")
+
+print("make your choice...")
+
+path=input("first, second or third")
+
+if (path=="first"):
+  print("okey lets see how you do")
+  print("you have started walking")
+  print("and a few small rocks have fallen")
+  print("everything seems fine")
+  print("time has passed")
+  print("and everything is going fine")
+  print("now you can start to see the end of the rocky path")
+  print("you start running to get to the end")
+  print("you have got this!")
+  print("you are almost there!")
+  print("keep goning, just a few more steps")
+  print("congrats, you are alive")
+
+if (path=="second"):
+  print("lets see what is coming")
+  print("now lets get you in the boat")
+  print("now start paddling")
+  print("everything seems normal")
+  print("the water has no strong current")
+  print("now you hear a string noise of water falling...")
+  print("you see up ahead a water fall")
+  print("you start to panic and try to go against the current")
+  print("you have noticed its not working")
+  print("so you just start to get ready to swim")
+  print("you have jumped of the boat and let the current take you towards the water fall")
+  print("you have fallen of the water fall")
+  print("you are trying to get to the surface")
+  print("finally you get to the surface and start to get out of the water")
+  print("now that you are out keep following the normal path")
+
+if (path==("third")):
+ print("okey lets start walking and see what happends")
+ print("you have started listening to snake sounds")
+ print("you start to get scared and you start running")
+ print("as you run snakes start chasing you")
+ print("they are catching up on you")
+ print("you keep running and they are getting closer")
+ print("now you start to see the way out of the path")
+ print("come on you have got this!")
+ print("keep running you are almost there")
+ print("you can do this!!")
+ print("finally you got out of the snake path")
+ print("now you can start walking ")
+ print("finally safe again")
+ print("keep going your way")
+
+print("now that you are safe from everything ")
+
+print("lets look at the map again")
+
+print("okey, if you follow the river of death souls")
+
+print("you will find your way back home")
+
+print("it will take a few days maybe weeks")
+
+print("but i know you can do it!")
+
+print("lets start going")
+
+print("im sorry to have to tell you that you have lost you animal so now you will have to walk")
+
+print("i know it is going to be a long journey but im sure you can make it back home")
+
+print("lets keep waliking...")
+
+print("you start to smell like something is roten")
+
+print("im sure it is the river of death souls")
+
+print("cover your nose or else you will get poisoned")
+
+for c in range(10):
+  nariz=input("click on the letter b to cover your nose")
+  if (nariz!="b"):
+    print("not a very smart choice, lets see where this takes us")
+    continue
+    
+print("finally you have got it right")
+
+print("but there is something i have to tell you...") 
+
+print("now you are going to be walking in a slower pace")
+  
+print("you will still get home but it will take more time")
+
+print("look ahead there is a safe box not that far")
+
+print("lets go see what it has inside")
+
+print("you have to figure out the password")
+
+while True:
+  code=input("try to guess the password")
+  if code=="Cr4zy":
+    print("good job")
+    break
+  else:
+    print("try again")
+
+print("im proud of you, you have survided all these time")
+
+print("i know we are close to your home")
+  
+print("just keep walking next to the river")
+
+print("a few time has passed and now you are getting away from the river that means that you are getting home in no time")
+
+print(".......")
+
+print("a few time has passed and yu start to feel a warm weather that means you are getting home very soon")
+
+print("just keep walking")
+
+print("i can feel we are getting very close")
+
+print("it is getting very hot")
+
+print("you are getting dehydrated")
+
+print("you start to get dizzy and you seat down")
+
+print("you have decided to drink some water")
+
+print("now you are still seating to see if you can start to feel better")
+
+print("lets wait a few more minutes")
+
+print("......")
+
+print("okay, now lets keep walking")
+
+print("......")
+
+print("okay, now lets keep walking")
+
+print("you start getting excited")
+
+print("you can start to see your house")
+
+print("you start to jump of excitement ")
+
+print("you start to see your neighbors, your town and your family")
+
+print("the parents of your friends ask you where their sons are")
+
+print("you put a sad face and tell them...")
+
+print("you tell them that they did not survive the adventure")
+
+print("they all start to cry and you hug them all")
+
+print("you tell them what happened during the adventure")
+
+print("they get proud because their sons died in a honorable way")
+
+print("you tell them that you want to go home to rest so you can sleep and eat something")
+
+print("you start walinkg towards your house...")
+
+print("when you get home you finally explain everything to your family")
+
+print("you decide to go to your room to get a little bit of sleep")
+
+print("when you get to your bed you lay down and think about all that has happened in the past days")
+
+print("you feel safe and proud because you have done a lot of things that you could have never done")
+
+print("you start to fall asleep")
+
+print("and as you fall asleep you keep thinking about your days away")
+
+print("finally you fall asleep and start dreaming")
+
+print("the game has ended")
+
+print("thanks for playing ROSABELLO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
